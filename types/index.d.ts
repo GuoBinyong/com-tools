1fff132a8a1f3b189c227df93c5cd1ba9a55ab56
郭斌勇 <guobinyong@qq.com>
2020-06-11 11:40:17 +0800

    重新构建

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+export * from "./oriTools";
+export * from "./tools";
---
+++ types/tools.d.ts
@@ -0,0 +1,14 @@
+/**
+ * 由于 eval() 有 安全 和 性能 的问题，所以封装了本函数 来 替代 eval()
+ * 本方法采用 Function 的方式 来实现 eval() 的效果；
+ * @param code
+ */
+export declare function betterEval(code: string): any;
+export declare function copyFunction<F extends Function>(fun: F): F;
+/**
+ * 根据函数字符串 和 函数名字 来创建函数
+ * @param funString:string   必选； 函数的代码字符串，必须是完整的函数代码字符串，不能是函数体的代码片断，可通过函数的 toString() 方法获得；
+ * @param name?:string      可选； 函数的名字；
+ * @param asDefault?:boolean    可选；name 参数是否作为函数名字的默认值（当 funString 中不包含函数名字时，再使用 name 作为函数的名字）
+ */
+export declare function createFunctionBy(funString: string, name?: string, asDefault?: boolean): Function;
