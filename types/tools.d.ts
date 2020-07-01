30f722f8ef1bb4557d9a925d61a48f1779df2bca
郭斌勇 <guobinyong@qq.com>
2020-07-01 13:38:51 +0800

    增加createSingleton

--- types/tools.d.ts
+++ types/tools.d.ts
@@ -17,3 +17,9 @@ export declare function copyFunction<F extends Function>(fun: F): F;
  * @param asDefault?:boolean    可选；name 参数是否作为函数名字的默认值（当 funString 中不包含函数名字时，再使用 name 作为函数的名字）
  */
 export declare function createFunctionBy(funString: string, name?: string, asDefault?: boolean): Function;
+/**
+ * 为指定的函数 fun 创建对应的 单例函数
+ * @param fun : Function  普通函数 或者 构造函数
+ * @return Function  增加了单例机制的函数；该函数的使用方式 和 原函数 fun 一样，只是无论多少次调用该函数，该函数都会返回原函数 fun 的同一实例
+ */
+export declare function createSingleton<Fun extends Function>(fun: Fun): Fun;
